9999e2e3cc8737ead8b3c1dacd6a248d64223957
beka-tech <abeka039@gmail.com>
2022-12-13 10:45:33 +0300

    Added Project

---
+++ Day 7/Whisper.js
@@ -0,0 +1,12 @@
+// Whisper
+// Write a function whisper that takes in a string and returns a "whispered" version of that string.
+
+// HINT: try using the .toLowerCase() method on a string to see what it does! You may also want to look up .toLowerCase() in the MDN documentation.
+
+function whisper(str) {
+    // your code here...
+    return str.toLowerCase();
+  }
+  
+  console.log(whisper("Hey Buddy")); // => "...hey buddy..."
+  console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
---
+++ day4 function.js
@@ -0,0 +1,15 @@
+// function square (x) {
+//     return x * x;
+//     };
+//     console.log(square(12));
+
+// function average (num1,num2,num3){
+//     return (num1 + num2*num3)/2;
+// };
+// console.log(average(39,4,8));
+
+function add(firstparameter, secondparameter) {
+    console.log(firstparameter + secondparameter);
+    
+}
+add(1,2);
