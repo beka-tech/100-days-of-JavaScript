18ddf6692f197ae7d8431042f578bf45c3654577
beka-tech <abeka039@gmail.com>
2023-01-10 21:28:33 +0300

    Day 15

---
+++ 100day of javascript/day15/script.js
@@ -0,0 +1,18 @@
+const hummus = function(factor){
+    const ingredient = function (amount, unit, name) {
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1){
+            unit += "s";
+        }
+
+        console.log(`${ingredientAmount} ${unit} ${name}`);
+        ingredient(1,"can","chickpeas");
+        ingredient(0.25,"cup","tahini");
+        ingredient(0.25,"cup","lemon juice");
+        ingredient(1,"clove","garlic");
+        ingredient(1,"tablespoon","olive oil");
+        ingredient(1,"teaspoon","cumin");
+
+        
+    };
+}
